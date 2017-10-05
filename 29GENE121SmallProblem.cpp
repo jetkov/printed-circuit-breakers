e90ff2ed96c587b8804102a00d8efa4fcd8896b9
Alex Petkovic <apetkovic22@gmail.com>
2017-10-05 12:15:40 -0400

    sdf

--- 29GENE121SmallProblem.cpp
+++ 29GENE121SmallProblem.cpp
@@ -16,11 +16,11 @@ double yEnd(double yStart, double radius, double angle) {
 }
 
 const double PCYLINDER_MAX = 33, BASE_LENGTH = 20; // cm
-const double XY_TOL = 0;
+const double XY_TOL = 0, targetX2 = 0, targetY2 = 0;
 
 int main() {
 	
-	double L = 0, R1 = 0, R2 = 0, angle2 = 0;
+	double L = 0, R1 = 0, R2 = 0, angle2 = 0, x2 = 0, y2 = 0;
 	
 	cin >> L >> R1 >> R2 >> angle2;
 	
@@ -29,6 +29,13 @@ int main() {
 		// calculate the resulting (x2,y2)
 	}
 
-	
+	if (fabs(targetX2 - x2) <= XY_TOL && fabs(targetY2 - y2) <= XY_TOL) 
+	{
+		printf("\nL: %d, R1: %d, R2 %d, Angle 2: %d", L, R1, R2, angle2);
+		printf("\nX2: %d, Y2: %d", x2, y2);
+	} else 
+	{
+		cout << "X2 and Y2 not within tolerance."
+	}
 
 }
