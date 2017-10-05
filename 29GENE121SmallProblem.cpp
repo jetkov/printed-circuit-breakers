0bc9dc9f5297ae3ab83510560c0e04ddff038fb1
Alex Petkovic <apetkovic22@gmail.com>
2017-10-05 11:46:37 -0400

    init

---
+++ 29GENE121SmallProblem.cpp
@@ -0,0 +1,34 @@
+// TRON Days Group #29
+
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+
+// Accepts a starting x value (xS), a radius (R), and an angle, and returns xE
+double xEnd(double xStart, double radius, double angle) {
+	return 0;
+}
+
+// Accepts a starting y value (yS), a radius (R), and an angle, and returns yE
+double yEnd(double yStart, double radius, double angle) {
+	return 0;
+}
+
+const double PCYLINDER_MAX = 33, BASE_LENGTH = 20; // cm
+const double XY_TOL = 0;
+
+int main() {
+	
+	double L = 0, R1 = 0, R2 = 0, angle2 = 0;
+	
+	cin >> L >> R1 >> R2 >> angle2;
+	
+	if (true) // Valid lengths L, R1, and R2, and valid angle 2
+	{
+		// calculate the resulting (x2,y2)
+	}
+
+	
+
+}
