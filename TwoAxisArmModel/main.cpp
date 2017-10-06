deb7ac16c7966ff4cd5ca5da230d1e9a708b43dd
Alex Petkovic <ajetkov@gmail.com>
2017-10-06 01:13:02 -0400

    Fixies

--- TwoAxisArmModel/main.cpp
+++ TwoAxisArmModel/main.cpp
@@ -38,43 +38,47 @@ int ccc_win_main()
 	// Setting up output file
 	ofstream fout("model.txt");
 	
-	int targetX2[3] = {200, 330, 370};
-	int targetY2[3] = {100, 310, 450};
+	const int TARGET_X2[3] = {200, 330, 370};
+	const int TARGET_Y2[3] = {100, 310, 450};
+	const double X2Y2_TOL = 10;
 	
 	fout << "Target Points: \n";
 	for(int i = 0; i < 3; i++) {
-		fout << "\t(" << targetX2[i] << ", " << targetY2[i] << ") \n";             
+		fout << "(" << TARGET_X2[i] << ", " << TARGET_Y2[i] << ") \n";             
 	} 
-	fout << "\n\n";
+	fout << "\nTolerance: " << X2Y2_TOL << "\n\n\n";
 		
 	fout << "X2       Y2       L       R1       R2       Angle 1       Angle 2"
 		 << "\n\n";
 	
-	double x2y2Tol = 10;
-	int maxR1 = 240, maxR2 = 240, maxAngle2 = 90;
+	const int MAX_R1 = 240, MAX_R2 = 240, MAX_ANGLE2 = 90;
 	double xMid = 0, x2 = 0, yMid = 0, y2 = 0, angle1 = 0, angle2Rad = 0;
 	
 	Point pointOrigin(-5, 0), pointMid, pointEnd;
 	Line baseLine(Point(-5, 0), Point(5, 0));
 
-	for(int R1 = 80; R1 <= maxR1; R1 += 16) 
+	for(int R1 = 80; R1 <= MAX_R1; R1 += 16) 
 	{
-		for(int R2 = 64; R2 <= maxR2; R2 += 16) 
+		for(int R2 = 64; R2 <= MAX_R2; R2 += 16) 
 		{
 			for(int L = 80; L <= R1; L += 16) 
 			{
-				for(int angle2 = -90; angle2 <= maxAngle2; angle2++)
+				for(int angle2 = -90; angle2 <= MAX_ANGLE2; angle2++)
 				{
 					angle2Rad = angle2 / 180.0 * M_PI;
 					
 					for(int j = 0; j < 2; j++) 
 					{
-						if (j == 0) angle1 = minAngle1(L);
-						else angle1 = maxAngle1(L);
+						if (j == 0) {
+							angle1 = minAngle1(L);
+						}
+						else {
+							angle1 = maxAngle1(L);
+						}
 						
-						xMid = xEnd(0, R1, minAngle1(L));
+						xMid = xEnd(0, R1, angle1);
 						x2 = xEnd(xMid, R2, angle2);
-						yMid = yEnd(0, R1, minAngle1(L));
+						yMid = yEnd(0, R1, angle1);
 						y2 = yEnd(yMid, R2, angle2);
 						
 						Point pointMid(xMid/50 -5, yMid/50);
@@ -85,8 +89,8 @@ int ccc_win_main()
 						
 						for(int i = 0; i < 3; i++) 
 						{
-							if (fabs(targetX2[i] - x2) <= x2y2Tol 
-								&& fabs(targetY2[i] - y2) <= x2y2Tol) 
+							if (fabs(TARGET_X2[i] - x2) <= X2Y2_TOL 
+								&& fabs(TARGET_Y2[i] - y2) <= X2Y2_TOL) 
 							{
 								// Formatting to table and outputting data
 								fout << fixed << left 
@@ -95,7 +99,7 @@ int ccc_win_main()
 									<< setw(8) << L
 									<< setw(9) << R1
 									<< setw(9) << R2 
-									<< setw(13) << minAngle1(L) * 180 / M_PI
+									<< setw(13) << angle1 * 180 / M_PI
 									<< setw(13) << angle2 << endl;
 									
 								cwin << LR1 << LR2 << baseLine;
