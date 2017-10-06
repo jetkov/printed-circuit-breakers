2e5d4f7fec45c2ac3c6c0a901c831e6b71ab4d36
Alex Petkovic <apetkovic22@gmail.com>
2017-10-05 20:37:35 -0400

    more

--- 29GENE121SmallProblem.cpp
+++ 29GENE121SmallProblem.cpp
@@ -21,60 +21,73 @@ double yEnd(double yStart, double radius, double angle)
 // Calculates min value of angle 1
 double minAngle1(double L) 
 {
-	return acos((20 * 20 + L * L - 21 * 21)/(2 * 20 * L));
+	return acos((200 * 200 + L * L - 210 * 210)/(2 * 200 * L));
 }
 
 // Calculates the max value of angle 1
 double maxAngle1(double L)
 {
-	return acos((20 * 20 + L * L - 31 * 31)/(2 * 20 * L));
+	return acos((200 * 200 + L * L - 310 * 310)/(2 * 200 * L));
 }
 
 int main() 
 {
-	cout << "Enter target X2, target Y2, and tolerance: ";
-	double x2y2Tol = 0, targetX2 = 0, targetY2 = 0;
-	cin >> targetX2 >> targetY2 >> x2y2Tol;
+	int targetX2[3] = {20, 33, 37};
+	int targetY2[3] = {10, 31, 45};
 	
-	int R1 = 80, maxR1 = 240, R2 = 64, maxR2 = 240, 
-		L = 80, maxL = maxR1, Angle2 = -90, maxAngle2 = 90;
+	int x2[3] = {0, 0, 0};
+	int y2[3] = {0, 0, 0};
+	
+	cout << "Enter tolerance: ";
+	double x2y2Tol = 0;
+	cin >> x2y2Tol;
+	
+	int maxR1 = 240, maxR2 = 240, maxAngle2 = 90;
 		
-	int x2 = 0, y2 = 0;
+	double x2 = 0, y2 = 0;
 	
-	for(; R1 <= maxR1; R1 += 16) 
+	boolean reachesAllPoints = false;
+	
+	for(int i = 0; i < 4; i++)
 	{
-		for(; R2 <= maxR2; R2 += 16) 
+		for(int R1 = 80; R1 <= maxR1; R1 += 16) 
 		{
-			for(; L <= maxL; L += 16) 
+			for(int R2 = 64; R2 <= maxR2; R2 += 16) 
 			{
-				for(; Angle2 <= maxAngle2; Angle2 += 1)
+				for(int L = 80; L <= R1; L += 16) 
 				{
-					// Testing with minAngle1 
-					x2 = cos(minAngle1(L) * R1 + cos(Angle2) * R2);
-					y2 = sin(minAngle1(L) * R1 + sin(Angle2) * R2);
-					
-					cout << x2 << " " << y2 << endl;
-					
-					if (fabs(targetX2 - x2) <= x2y2Tol && fabs(targetY2 - y2) <= x2y2Tol) 
+					for(int Angle2 = 0; Angle2 <= maxAngle2; Angle2++)
 					{
-						printf("\nL: %d, R1: %d, R2 %d, Angle 2: %d", L, R1, R2, Angle2);
-						printf("\nX2: %d, Y2: %d", x2, y2);
-					} 
-					
-					// Testing with maxAngle1 
-					x2 = cos(maxAngle1(L) * R1 + cos(Angle2) * R2);
-					y2 = sin(maxAngle1(L) * R1 + sin(Angle2) * R2);
-					
-					cout << x2 << " " << y2 << endl;
-					
-					if (fabs(targetX2 - x2) <= x2y2Tol && fabs(targetY2 - y2) <= x2y2Tol) 
-					{
-						printf("\nL: %d, R1: %d, R2 %d, Angle 2: %d", L, R1, R2, Angle2);
-						printf("\nX2: %d, Y2: %d", x2, y2);
-					} 
-				}
-			} 
+						
+						double Angle2R = Angle2 / 180.0 * M_PI;
+						
+						// Testing with minAngle1
+						
+						x2[i] = xEnd(xEnd(0, R1, minAngle1(L)), R2, Angle2);
+						y2[i] = yEnd(yEnd(0, R1, minAngle1(L)), R2, Angle2);
+			
+			
+						if (fabs(targetX2 - x2) <= x2y2Tol && fabs(targetY2 - y2) <= x2y2Tol) 
+						{
+							printf("\nL: %d, R1: %d, R2 %d, Angle 1: %f Angle 2: %f", L, R1, R2, minAngle1(L) * 180 / M_PI, Angle2);
+							printf("\nX2: %f, Y2: %f \n", x2, y2);
+						} 
+						
+						// Testing with maxAngle1
+						
+						x2 = xEnd(xEnd(0, R1, maxAngle1(L)), R2, Angle2);
+						y2 = yEnd(yEnd(0, R1, maxAngle1(L)), R2, Angle2);
+			
+						if (fabs(targetX2 - x2) <= x2y2Tol && fabs(targetY2 - y2) <= x2y2Tol) 
+						{
+							printf("\nL: %d, R1: %d, R2 %d, Angle 1: %f Angle 2: %f", L, R1, R2, maxAngle1(L) * 180 / M_PI, Angle2);
+							printf("\nX2: %f, Y2: %f \n", x2, y2);
+						} 
+					}
+				} 
+			}
 		}
+		
 	}
 	
 	return EXIT_SUCCESS;	
